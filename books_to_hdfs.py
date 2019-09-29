8e592f3d6b61530566b7ef6a65c475f69e5406e3
Daniel Nikulin <danil.nikulin@gmail.com>
2019-09-29 22:08:47 +0300

    -refactor structure

--- books_to_hdfs.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'Daniil Nikulin'
-__license__ = "Apache License 2.0"
-__email__ = "danil.nikulin@gmail.com"
-__date__ = "2019-09-23"
-__app__ = "mp_donntu_spark_hdfs"
-__status__ = "Development"
